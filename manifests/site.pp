ddae09386744ba7619592c92e3aeb619ad5cfa9a
Matt Fellows <matt.fellows@onegeek.com.au>
2014-05-06 00:38:52 +1000

    Updated node modules with ionic and test frameworks. Updated ruby gems (sass, compass). Added tmp build dir for archive installs

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,6 +12,7 @@ Exec {
     "${boxen::config::home}/rbenv/bin",
     "${boxen::config::home}/rbenv/plugins/ruby-build/bin",
     "${boxen::config::home}/homebrew/bin",
+    '/opt/bin',
     '/usr/bin',
     '/bin',
     '/usr/sbin',
@@ -27,6 +28,13 @@ Exec {
 File {
   group => 'staff',
   owner => $boxen_user
+}
+
+# For build artifacts to avoid re-downloading
+file { '/opt/src':
+  ensure  => directory,
+  owner   => $boxen_user,
+  group   => 'staff'
 }
 
 Package {
@@ -71,26 +79,46 @@ node default {
   include nodejs::v0_8
   include nodejs::v0_10
 
-  # default ruby versions
+  # Set the global default ruby (auto-installs it if it can)
+  $ruby_version = '2.0.0-p451'
+  class { 'ruby::global':
+    version => $ruby_version
+  }
+
+  # Install other Rubies
   ruby::version { '1.9.3': }
   ruby::version { '2.0.0': }
   ruby::version { '2.1.0': }
   ruby::version { '2.1.1': }
+
+  ruby::gem { "compass for ruby ${ruby_version}":
+    gem           => 'compass',
+    version       => '~> 0.12.6',
+    ruby          => $ruby_version,
+  }
+
+  # Setup alternative bin dir
+  file { '/opt/bin':
+    ensure  => directory,
+    owner   => $boxen_user,
+    group   => 'staff'
+  }
 
   # common, useful packages
   package {
     [
       'ack',
       'findutils',
-      'gnu-tar'
+      'gnu-tar',
     ]:
   }
+
+  include wget
 
   file { "${boxen::config::srcdir}/our-boxen":
     ensure => link,
     target => $boxen::config::repodir
   }
-
 
   # Matt's stuff
   include virtualbox
@@ -98,6 +126,42 @@ node default {
   include sublime_text_2
   include iterm2::stable
 
+  # For cordova / java
+  $ant_version = "apache-ant-1.9.3"
+
+  file { "/opt/${ant_version}/":
+    ensure  => directory
+  }
+
+  archive { 'ant':
+    ensure      => present,
+    url         => "http://mirror.rackcentral.com.au/apache/ant/binaries/${ant_version}-bin.tar.gz",
+    target      => "/opt/",
+    checksum    => false,
+    # Change src dir to speed things up after a restart?
+    src_target  => '/opt/src/',
+    timeout     => '300'
+  }
+
+  file { '/opt/bin/ant':
+    ensure    => link,
+    target    => "/opt/${ant_version}/bin/ant"
+  }
+
+  #
+  # Game Dev
+  #
+  include android::sdk
+  include android::tools
+  include android::platform_tools
+  # android::build_tools
+
+  # Consider pkgdmg as provider?
+  package { 'android-intel-HAXM':
+    # provider => 'appdmg',
+    provider => 'pkgdmg',
+    source => 'https://software.intel.com/sites/default/files/managed/68/45/haxm-macosx_r04.zip'
+  }
 
   # Consider pkgdmg as provider?
   package { 'unity-4.3.4':
@@ -151,6 +215,7 @@ node default {
   }
 
   # IDE
+  include webstorm
   class { 'intellij':
    edition => 'community',
   }
@@ -176,7 +241,9 @@ node default {
     url         => 'http://plugins.jetbrains.com/files/1347/15875/scala-intellij-bin-0.35.683.zip',
     checksum    => false,
     target      => "${intellij_plugin_dir}",
-    src_target  => '/tmp',
+
+    # Change src dir to speed things up after a restart?
+    src_target  => '/opt/src',
     timeout     => '300'
   }
   # archive { 'intellij-ruby-plugin':
@@ -188,8 +255,10 @@ node default {
   # }
 
   # Browsers
-  include chrome
-  include firefox
+  # include chrome
+  # include firefox
+  include firefox::nightly
+  include chrome::canary
 
   # Vagrant
   include vagrant
@@ -216,13 +285,14 @@ node default {
       'ctags',
       'ruby-build',
       'gnu-sed', # replace useless sed that comes with OSX
+      'bash-completion',
     ]:
   }
 
-  package {
-    'bash-completion':
-      ensure => present
-  }
+
+  # package { 'compass':
+  #   provider => 'gem'
+  # }
 
   # OS Customizations
   include osx::global::expand_save_dialog
@@ -360,27 +430,9 @@ node default {
 
   ### Bower, yeoman, s8 ....
   # install some npm modules
-  nodejs::module { 'grunt':
+
+  nodejs::module { ['cordova', 'ionic', 'ios-sim', 'ripple', 'grunt', 'generator-ionic', 'karma', 'karma-jasmine', 'express', 'bower', 'yeoman']:
     node_version => 'v0.10.26'
   }
 
-  nodejs::module { 'express':
-    node_version => 'v0.10.26'
-  }
-
-  nodejs::module { 'bower':
-    node_version => 'v0.10.26'
-  }
-
-  nodejs::module { 'yeoman':
-    node_version => 'v0.10.26'
-  }
-
-  # nodejs::module { 'angular':
-  #   node_version => 'v0.10.26'
-  # }
-
-  # nodejs::module { 'generator-scalatra':
-  #   node_version => 'v0.10.26'
-  # }
 }
